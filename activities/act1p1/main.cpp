b382b384a0dad8df8f860be42a1f4baa762f86f7
Jeshua N <pigihunter@gmail.com>
2022-08-18 22:11:56 -0500

    Added string and csrdlib library. Program now stores the inputs values to a vector, and for now we print them

--- activities/act1p1/main.cpp
+++ activities/act1p1/main.cpp
@@ -9,13 +9,22 @@ Fecha de creación y modificación: 18/08/2022 - XX/08/2022
 #include <vector>
 #include <fstream>
 #include <math.h>
+#include <string>
+#include <cstdlib>
 
 int main (int argc, char *argv[]) {
+    std::vector<int> list;
     // Revisamos si vamos a obtener nuestra instancia de un txt o de inputs.
     if (argc > 2) {
         std::cout << "Inputs" << std::endl;
+        for (int i = 1; i < argc; i++) {
+            list.push_back(std::atoi(argv[i]));
+        }
     } else {
         std::cout << "file" << std::endl;
+    }
+    for (int i = 0; i < list.size(); i++) {
+        std::cout << list[i] << " ";
     }
     return 0;
 }
