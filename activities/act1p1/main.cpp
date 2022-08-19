7ac87c362bc7f867ec67d5e020fe95c1fced50d2
Jeshua N <pigihunter@gmail.com>
2022-08-18 21:47:03 -0500

    app verifies if our instances come from a txt file or inputs

--- activities/act1p1/main.cpp
+++ activities/act1p1/main.cpp
@@ -5,8 +5,18 @@ Descripción: Actividad para poner en práctica la técnica de progrmación
 Autor: Jeshua Nava Avila | A01639282
 Fecha de creación y modificación: 18/08/2022 - XX/08/2022
 */
+#include <iostream>
 
 int main (int argc, char *argv[]) {
-    
+    // Revisamos si vamos a obtener nuestra entrada de un txt o de inputs.
+    // Si la cantidad de argumentos es mayor a dos, significa que
+    //  obtuvimos nuestra entrada a base de inputs. De lo contrario,
+    //  si solamente tenmos dos argumentos, significa que recibimos un
+    //  txt. 
+    if (argc > 2) {
+        std::cout << "Inputs" << std::endl;
+    } else {
+        std::cout << "file" << std::endl;
+    }
     return 0;
 }
