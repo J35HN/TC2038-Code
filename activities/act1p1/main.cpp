aa55d202f3f6a0ddf229e0018bc586cf273b447d
Jeshua N <pigihunter@gmail.com>
2022-08-18 21:19:29 -0500

    Created folder for activity 1.1 . Basic information added in main.cpp

---
+++ activities/act1p1/main.cpp
@@ -0,0 +1,12 @@
+/*
+Nombre: Actividad 1.1 - Implementación de la técnica de programacion "divide y vencerás"
+Descripción: Actividad para poner en práctica la técnica de progrmación 
+                "Divide y Vencerás".
+Autor: Jeshua Nava Avila | A01639282
+Fecha de creación y modificación: 18/08/2022 - XX/08/2022
+*/
+
+int main (int argc, char *argv[]) {
+    
+    return 0;
+}
