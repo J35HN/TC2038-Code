fda72bf19ea5f6da2e4ba45c179f864340e21006
Jeshua N <pigihunter@gmail.com>
2022-08-18 22:27:04 -0500

    program now accepts txt files as arguments in command line

--- activities/act1p1/main.cpp
+++ activities/act1p1/main.cpp
@@ -14,7 +14,10 @@ Fecha de creación y modificación: 18/08/2022 - XX/08/2022
 
 int main (int argc, char *argv[]) {
     std::vector<int> list;
+    std::string fn = "waiting for file name :)";
+    int number;
     // Revisamos si vamos a obtener nuestra instancia de un txt o de inputs.
+    // No importa el caso, insertamos en la lista "list" los números.
     if (argc > 2) {
         std::cout << "Inputs" << std::endl;
         for (int i = 1; i < argc; i++) {
@@ -22,6 +25,18 @@ int main (int argc, char *argv[]) {
         }
     } else {
         std::cout << "file" << std::endl;
+        fn = std::string(argv[1]); // nombre del archivo.
+        std::ifstream input_file(fn);
+        // Verificamos si pudimos abrir el archivo.
+        if (!input_file.is_open()) {
+            std::cerr << "No fue posible abrir el archivo - '"
+            << fn << "'" << std::endl;
+        return EXIT_FAILURE;
+        }
+        while (input_file >> number) {
+            list.push_back(number);
+        }
+        input_file.close();
     }
     for (int i = 0; i < list.size(); i++) {
         std::cout << list[i] << " ";
