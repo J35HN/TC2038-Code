75987bf5afb716aa3011cfa288394ba0ce631874
Jeshua N <pigihunter@gmail.com>
2022-08-18 21:52:32 -0500

    Added vector, math and fstream library. Also, changed a comment

--- activities/act1p1/main.cpp
+++ activities/act1p1/main.cpp
@@ -6,13 +6,12 @@ Autor: Jeshua Nava Avila | A01639282
 Fecha de creación y modificación: 18/08/2022 - XX/08/2022
 */
 #include <iostream>
+#include <vector>
+#include <fstream>
+#include <math.h>
 
 int main (int argc, char *argv[]) {
-    // Revisamos si vamos a obtener nuestra entrada de un txt o de inputs.
-    // Si la cantidad de argumentos es mayor a dos, significa que
-    //  obtuvimos nuestra entrada a base de inputs. De lo contrario,
-    //  si solamente tenmos dos argumentos, significa que recibimos un
-    //  txt. 
+    // Revisamos si vamos a obtener nuestra instancia de un txt o de inputs.
     if (argc > 2) {
         std::cout << "Inputs" << std::endl;
     } else {
