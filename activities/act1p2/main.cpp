2c252e073bf6d713d15c38d618f05c4ca318efdc
Jeshua N <pigihunter@gmail.com>
2022-08-23 16:26:03 -0500

    Function readData created. Code now reads valid coins values

--- activities/act1p2/main.cpp
+++ activities/act1p2/main.cpp
@@ -10,12 +10,27 @@ Fecha de creación y modificación: 23/08/2022 - XX/08/2022
 #include <iostream>
 #include <vector>
 
+void readData (std::vector<int>& validCoins, int n)
+{
+    int data = 0;
+    for (int i = 0; i < n; i++)
+    {
+        std::cin >> data;
+        validCoins.push_back(data);
+    }
+}
+
 int main (int argc, char *argv[])
 {
-    std::vector<int> coins;
+    std::vector<int> validCoins;
     int n = 0;
-    // Read amount of arguments/valid coins inputs.
+    // Read amount of arguments/valid coins inputs, and the values of the coins.
     std::cin >> n;
-    std::cout << "Amount of valid coins: " << n << std::endl;
+    readData(validCoins, n);
+    std::cout << "Amount of valid coins: " << n << "\nValid coins: " << std::endl;
+    for (int i = 0; i < n; i++)
+    {
+        std::cout << validCoins[i] << " ";
+    }
     return 0;
 }
