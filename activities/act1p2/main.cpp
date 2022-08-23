8dd8270f97a0e9923aa32b6b65bd4b9996531db7
Jeshua N <pigihunter@gmail.com>
2022-08-23 16:20:41 -0500

    Code now read amount of valid coins

--- activities/act1p2/main.cpp
+++ activities/act1p2/main.cpp
@@ -12,5 +12,10 @@ Fecha de creación y modificación: 23/08/2022 - XX/08/2022
 
 int main (int argc, char *argv[])
 {
+    std::vector<int> coins;
+    int n = 0;
+    // Read amount of arguments/valid coins inputs.
+    std::cin >> n;
+    std::cout << "Amount of valid coins: " << n << std::endl;
     return 0;
 }
