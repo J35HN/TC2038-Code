454d1e4e34d71d0d2cf0d47370facb2bcc009124
Jeshua N <pigihunter@gmail.com>
2022-08-23 16:03:37 -0500

    Added iostream and vector library, a basic main function and a introduction of the activity

---
+++ activities/act1p2/main.cpp
@@ -0,0 +1,16 @@
+/*
+Nombre: Actividad 1.2 -    
+        Implementación de la técnica de programación 
+        "Programación dinámica" y "algoritmos avaros"
+Descripción: Actividad para generar práctica de "Programación Dinámica"
+                y "Algoritmos Avaros".
+Autor: Jeshua Nava Avila | A01639282
+Fecha de creación y modificación: 23/08/2022 - XX/08/2022
+*/
+#include <iostream>
+#include <vector>
+
+int main (int argc, char *argv[])
+{
+    return 0;
+}
