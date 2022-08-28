6d8cff3382f12d444205e5774b2a3525acc99455
Jeshua N <pigihunter@gmail.com>
2022-08-28 11:27:22 -0500

    Implemented merge sort

--- activities/act1p2/main.cpp
+++ activities/act1p2/main.cpp
@@ -9,6 +9,7 @@ Fecha de creación y modificación: 23/08/2022 - XX/08/2022
 */
 #include <iostream>
 #include <vector>
+#include <math.h>
 
 /**
  * @brief Read coin data from txt file that has been passed as an argument.
@@ -27,21 +28,109 @@ void readData (std::vector<int>& validCoins, int N)
     }
 }
 
+/**
+ * @brief merge algorith from Merge Sort. Sorts from largest to smalles value.
+ * 
+ * @param list List that contains the data and that will be sorted with other two sub-vectors.
+ * @param leftIndex Indicates the numeric value of the starting index of the list.
+ * @param middleIndex Indicates the numeric value of the middle index of the list.
+ * @param rightIndex Indicates the numeric value of the end or last possible index of the list.
+ * Complexity: O(n).
+ */
+void merge (std::vector<int>& list, int leftIndex, int middleIndex, int rightIndex)
+{
+    std::vector<int> leftList, rightList; // Temporal sub-vectors. (STARTING WITH NO VALUE).
+    int leftSize = 0, rightSize = 0, i = 0, j = 0;
+    int trueIndex = leftIndex;
+    // Calculte the "length" of the sub-vectors. They end up size + 1, to compensate
+    //      for 0-indexing in cpp.
+    leftSize = middleIndex - leftIndex + 1;
+    rightSize = rightIndex - middleIndex;
+    // Copy corresponding values of list to the sub-vectors.
+    for (int i = 0; i < leftSize; i++)
+    {
+        leftList.push_back(list[leftIndex + i]);
+    }
+    for (int j = 0; j < rightSize; j++)
+    {
+        rightList.push_back(list[middleIndex + j + 1]);
+    }
+    i = j = 0; // Reset indexes for independent use in left and right sub-vectors.
+    // Compare and add numbers to our list.
+    while (i < leftSize && j < rightSize)
+    {
+        if (leftList[i] >= rightList[j])
+        {
+            list[trueIndex] = leftList[i];
+            i++;
+        }
+        else
+        {
+            list[trueIndex] = rightList[j];
+            j++;
+        }
+        trueIndex++;
+    }
+    // If any elements are left in our sub-vectors, we add them to our list.
+    while (j < rightSize)
+    {
+        list[trueIndex] = rightList[j];
+        j++;
+        trueIndex++;
+    }
+    while (i < leftSize)
+    {
+        list[trueIndex] = leftList[i];
+        i++;
+        trueIndex++;
+    }
+    
+}
+
+/**
+ * @brief Merge Sort algorithm. Divides the list into sub-vectors, and sorts from largest to smallest.
+ * 
+ * @param list The list to sort. 
+ * @param leftIndex The value of the starting index of the list.
+ * @param rightIndex The value of the last index of the list.
+ * Complexity: O(n log(n)).
+ */
+void mergeSort (std::vector<int>& list, int leftIndex, int rightIndex)
+{
+    int middleIndex = 0;
+    // Verify if we need to create more sub-problems.
+    if (leftIndex < rightIndex)
+    {
+        middleIndex = std::floor((leftIndex + rightIndex) / 2); 
+        mergeSort(list, leftIndex, middleIndex);
+        mergeSort(list, middleIndex + 1, rightIndex);
+
+        merge(list, leftIndex, middleIndex, rightIndex);
+    }
+    
+}
+
 int main (int argc, char *argv[])
 {
-    std::vector<int> validCoins;
+    std::vector<int> denominationCoins;
     int N = 0, P = 0, Q = 0;
+    std::vector<int> greedySolution;
+    std::vector<int> dynamicSolution;
     // Read amount of arguments/valid coins inputs, and the values of the coins.
     std::cin >> N;
-    readData(validCoins, N);
+    readData(denominationCoins, N);
+    // Order from biggest to smallest our denommination list (just in case).
+    mergeSort(denominationCoins, 0, denominationCoins.size() - 1);
     // Read product price and payment.
     std::cin >> P;
     std::cin >> Q;
+    /*
     std::cout << "Amount of valid coins: " << N << "\nValid coins: " << std::endl;
     for (int i = 0; i < N; i++)
     {
         std::cout << validCoins[i] << " ";
     }
     std::cout << "\nPrice of product: " << P << "\nPayment: " << Q << std::endl;
+    */
     return 0;
 }
