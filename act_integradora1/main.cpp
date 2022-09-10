319b2719041bc26354c64b7bc6609a853f36667e
Jeshua N <pigihunter@gmail.com>
2022-09-10 18:05:26 -0500

    Started with Actividad Integradora

---
+++ act_integradora1/main.cpp
@@ -0,0 +1,15 @@
+/*
+Nombre: Actividad Integradora 1.
+Descripción: Primera actividad integradora, involucrando 
+                algoritmos de busqueda de strings.
+Autor: Jeshua Nava Avila | A01639282
+Fecha de creación y modificación: 10/09/2022 - 30/09/2022
+*/
+#include <iostream>
+#include <vector>
+#include <math.h>
+
+int main (int argc, char *argv[])
+{
+    return 0;
+}
