6346c943a8fb51f8d2850fa61b48a8273dbd0497
Jeshua N <pigihunter@gmail.com>
2022-09-10 18:26:22 -0500

    Program now generates random hexadecimal chars. Changed some variable names for better description and imported new libraries

--- generateFile.cpp
+++ generateFile.cpp
@@ -1,12 +1,24 @@
 #include <iostream>
 #include <fstream>
 #include <string.h>
+#include <stdlib.h>
+#include <time.h>
 
 int main (int argc, char *argv[])
 {
+    std::string hexadecimalChars[17] = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "0", 
+        "A", "B", "C", "D", "E", "F", "\n"};
     std::string outputFileName = std::string(argv[1]);
-    int linesToGenereta = stoi(std::string(argv[2]));
+    int charsToGenerate = stoi(std::string(argv[2]));
 
+    // Initialize random seed.
+    std::srand (time(NULL));
+
+    // Generate random chars.
+    for (int i = 0; i < charsToGenerate; i++)
+    {
+        std::cout << hexadecimalChars[rand()%17];
+    }
     //std::cout << "Filename: " << outputFile << ". Lines: " << linesToGenereta << std::endl;
 
     return 0;
