a78ce9e5e02cac33ce7ba9208957ac985cffccd0
Jeshua N <pigihunter@gmail.com>
2022-09-10 18:13:31 -0500

    Created generateFile.cpp to generate txt files with random hexadecimal characters

---
+++ generateFile.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <fstream>
+#include <string.h>
+
+int main (int argc, char *argv[])
+{
+    std::string outputFileName = std::string(argv[1]);
+    int linesToGenereta = stoi(std::string(argv[2]));
+
+    //std::cout << "Filename: " << outputFile << ". Lines: " << linesToGenereta << std::endl;
+
+    return 0;
+}
