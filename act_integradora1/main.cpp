bfaa2f24de847dd94424960c879a665060c7a8b0
Jeshua N <pigihunter@gmail.com>
2022-09-11 10:51:10 -0500

    copypasta of manacher algorithm

--- act_integradora1/main.cpp
+++ act_integradora1/main.cpp
@@ -37,6 +37,58 @@ int openFileAndStoreInVar (std::string& str, std::string path)
         }
 }
 
+std::string convertString (std::string str)
+{
+    std::string newStr = "@";
+    for (int i = 0; i < str.size(); i++)
+    {
+        newStr += "#" + str.substr(i, 1);
+    }
+
+    newStr += "#$";
+    return newStr;
+}
+
+std::string longestPalindromeSubStr(std::vector<int>& indexArray, std::string str)
+{
+    std::string newStr = convertString(str);
+    int center = 0, right = 0;
+    for (int i = 1; i < newStr.size() - 1; i++)
+    {
+        int iMirror = center - (i - center);
+
+        if (right > i)
+        {
+            indexArray[i] = std::min(right - i, indexArray[iMirror]);
+        }
+
+        while (newStr[i + 1 + indexArray[i]] == newStr[i - 1 - indexArray[i]])
+        {
+            indexArray[i] = indexArray[i] + 1;
+        }
+
+        if (i + indexArray[i] > right)
+        {
+            center = i;
+            right = i + indexArray[i];
+        }
+    }
+
+    int maxPalindrome = 0;
+    int centerIndex = 0;
+
+    for (int i = 1; i < newStr.size() - 1; i++)
+    {
+        if (indexArray[i] > maxPalindrome)
+        {
+            maxPalindrome = indexArray[i];
+            centerIndex = i;
+        }
+    }
+    std::cout << maxPalindrome << " " << newStr[centerIndex] << std::endl;
+    return str.substr( (centerIndex - 1 - maxPalindrome) / 2, maxPalindrome);
+}
+
 int main (int argc, char *argv[])
 {
     bool mcodeInTransmission = false;
