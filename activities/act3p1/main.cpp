c844a99f3d8289beb64e748be5ef51e2a67c9b66
Jeshua N <pigihunter@gmail.com>
2022-10-05 22:06:43 -0500

    Created test.txt file, and main.cpp reads and stores data from test.txt

--- activities/act3p1/main.cpp
+++ activities/act3p1/main.cpp
@@ -10,5 +10,21 @@ Fecha de creación y modificación: 05/10/2022 - XX/10/2022
 
 int main(int argc, char *argv[])
 {
+    int n = 0;
+    std::string word;
+    std::vector<std::string> wordsInTrie;
+    // Read amount of words to insert.
+    std::cin >> n;
+    // Store n amount of words.
+    for (int i = 0; i < n; i++)
+    {
+        std::cin >> word;
+        wordsInTrie.push_back(word);
+    }
+    // Print results.
+    for (int i = 0; i < n; i++)
+    {
+        std::cout << wordsInTrie[i] << " ";
+    }
     return 0;
 }
