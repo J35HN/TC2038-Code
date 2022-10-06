79e570ca28dc3ac6707d1a21a0f4121a5fedd240
Jeshua N <pigihunter@gmail.com>
2022-10-05 21:50:20 -0500

    Created 3.1 Activity folder and main.cpp

---
+++ activities/act3p1/main.cpp
@@ -0,0 +1,12 @@
+/*
+Nombre: Actividad 3.1 - Implementación de "Tries".
+Descripción: Programa que implementa un Trie y verifica
+                su funcionalidad con inserción y busqueda.
+Autor: Jeshua Nava Avila | A01639282
+Fecha de creación y modificación: 05/10/2022 - XX/10/2022
+*/
+
+int main(int argc, char *argv[])
+{
+    return 0;
+}
