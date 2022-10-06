c2b7933e7cf5c10a6b37c93a9719db48691a4826
Jeshua N <pigihunter@gmail.com>
2022-10-05 22:14:44 -0500

    main.cpp reads and stores amount of words to search in a Trie

--- activities/act3p1/main.cpp
+++ activities/act3p1/main.cpp
@@ -20,17 +20,27 @@ void readData (std::vector<std::string>& list, int n)
 
 int main(int argc, char *argv[])
 {
-    int n = 0;
+    int n = 0, m = 0;
     std::string word;
     std::vector<std::string> wordsInTrie;
+    std::vector<std::string> wordsToSearch;
     // Read amount of words to insert.
     std::cin >> n;
     // Store n amount of words.
     readData(wordsInTrie, n);
+    // Read amount of words to search.
+    std::cin >> m;
+    // Store m amount of words.
+    readData(wordsToSearch, m);
     // Print results.
     for (int i = 0; i < n; i++)
     {
         std::cout << wordsInTrie[i] << " ";
+    }
+    std::cout << std::endl;
+    for (int i = 0; i < m; i++)
+    {
+        std::cout << wordsToSearch[i] << " ";
     }
     return 0;
 }
