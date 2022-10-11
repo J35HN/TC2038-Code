a61191248312ee37e5960a16b9a39d83f9b66d5f
Jeshua N <pigihunter@gmail.com>
2022-10-11 15:21:16 -0500

    imported work from Replit

---
+++ activities/act3p2/dijkstra.h
@@ -0,0 +1,9 @@
+#include <vector>
+
+using std::vector;
+
+vector<vector<int>> dijkstraAlgorithm(vector<vector<int>> graph){
+  vector<vector<int>> results;
+  results = graph;
+  return results;
+}
--- activities/act3p2/main.cpp
+++ activities/act3p2/main.cpp
@@ -4,7 +4,54 @@ Descripción:
 Autor: Jeshua Nava Avila | A01639282
 Fecha de creación y modificación: 07/10/2022 - XX/10/2022
 */
-int main(int argc, char *argv[])
-{
-    return 0;
+#include <iostream>
+#include <vector>
+
+#include "dijkstra.h"
+
+using std::cin;
+using std::cout;
+using std::vector;
+
+
+int main(int argc, char *argv[]) {
+  int n = 0;
+
+  // Read values from txt file.
+  cin >> n;
+  // Init square matrix and result matrix.
+  vector<vector<int>> matrix(n, vector<int>(n));
+  vector<vector<int>> dijkstraResultMatrix = matrix;
+
+  // insert values in matrix.
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < n; j++) {
+      cin >> matrix[i][j];
+    }
+  }
+
+  // Compute results.
+  dijkstraResultMatrix = dijkstraAlgorithm(matrix);
+  
+  // Print values.
+  /// Dijkstra
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < n; j++) {
+      // Do not print the values of the same node.
+      if (i != j) {
+      cout << "node " << i + 1 << " to node " << j + 1 << ": " <<  dijkstraResultMatrix[i][j] << std::endl;        
+      }
+    }
+  }
+
+  /*
+  for (int i = 0; i < n; i++) {
+    cout << std::endl;
+    for (int j = 0; j < n; j++) {
+      cout << " " << matrix[i][j];
+    }
+  }
+  */
+
+  return 0;
 }
