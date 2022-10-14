f1b14361cc2fe58b04d18c2086dfe748a17ba244
Jeshua N <pigihunter@gmail.com>
2022-10-13 21:17:03 -0500

    obtainNearedNode funciton implemented

--- activities/act3p2/dijkstra.h
+++ activities/act3p2/dijkstra.h
@@ -5,8 +5,30 @@ using std::vector;
 
 int INF = INT_MAX;
 
+int obtainNearestNode(int totalNodes, vector<bool> visitedNodes, vector<int> distances)
+{
+  int minDistance = INF;
+  int nearestNode = 0;
+  for (int i = 0; i < totalNodes; i++)
+  {
+    if (visitedNodes[i] != true && ( distances[i] < minDistance))
+    {
+      minDistance = distances[i];
+      nearestNode = i;
+    }
+  }
+  return nearestNode;
+}
+
 vector<int> dijkstraAlgorithm(vector<vector<int>> graph, int sourceNode){
-  vector<int> results( graph[0].size(), INF);
-  
-  return results;
+  vector<int> distanceResults(graph[0].size(), INF); // Distance values from source node to each index node.
+  vector<bool> visitedNodes (graph[0].size(), false); // Array to keep track of visited nodes.
+  distanceResults[sourceNode] = 0; // Distance from source node to itself is 0.
+  // Iterate to each node and calculate its distance.
+  for (int i = 0; i < graph[0].size(); i++)
+  {
+    int nearestNode = obtainNearestNode(graph[0].size(), visitedNodes, distanceResults);
+  }
+
+  return distanceResults;
 }
