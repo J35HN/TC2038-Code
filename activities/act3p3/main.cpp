cf23b2006fdfbb7f17443b2db5ef0a040e7f79ee
Jeshua N <pigihunter@gmail.com>
2022-10-15 11:42:34 -0500

    Started the implementation of 'Knapsack problem'

--- activities/act3p3/main.cpp
+++ activities/act3p3/main.cpp
@@ -1,10 +1,21 @@
 /*
 Nombre: Actividad 3.3 - Implementación de "Knapsack problem".
-Descripción: 
+Descripción: Programa que resuelva el problema de la mochila (Knapsack problem)
 Autor: Jeshua Nava Avila | A01639282
-Fecha de creación y modificación: 07/10/2022 - XX/10/2022
+Fecha de creación y modificación: 15/10/2022 - XX/10/2022
 */
+#include <iostream>
+#include <vector>
+
+using std::cin;
+using std::cout;
+using std::vector;
+
 int main(int argc, char *argv[])
 {
+    int N = 0, W = 0;
+    // Init vectors.
+    vector<int> valuesOfElements;
+    vector<int> weightsOfElements;
     return 0;
 }
