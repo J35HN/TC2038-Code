484cfc84c2777b06e56abf96d73a304cb3f2c79d
Jeshua N <pigihunter@gmail.com>
2022-11-08 14:39:57 -0600

    Update

--- activities/act4p2/Graham_Scan.h
+++ activities/act4p2/Graham_Scan.h
@@ -1,15 +1,40 @@
 #include <bits/stdc++.h>
+#include <stack>
 
 #include "Point.h"
 
 using std::cin;
 using std::cout;
 using std::endl;
+using std::stack;
 using std::vector;
 
 /**
+ * @brief Prints the elements of a stack.
+ *
+ * @param polygon Stack that contains the point describing a polygon.
+ * Complexity: O(n).
+ */
+void printStack(stack<Point> polygon) {
+  int polygonSize = polygon.size();
+  vector<Point> polygonCounterClockwise;
+
+  cout << "=== Convex Hull ===" << endl;
+
+  for (int i = 0; i < polygonSize; i++) {
+    polygonCounterClockwise.push_back(polygon.top());
+    polygon.pop();
+  }
+
+  for (int i = polygonSize - 1; i >= 0; i--) {
+    cout << polygonCounterClockwise[i].x << ", " << polygonCounterClockwise[i].y
+         << endl;
+  }
+}
+
+/**
  * @brief Swaps the values of two points.
- * 
+ *
  * @param a Point 1.
  * @param b Point 2.
  * Complexity: O(1).
@@ -21,9 +46,24 @@ void swap(Point &a, Point &b) {
 }
 
 /**
+ * @brief Returns a reference to the top object of a stack.
+ *
+ * @param points A stack ofpoints.
+ * @return A point object that is currently the top one of a stack.
+ * Complexity: O(1).
+ */
+Point nextToTop(stack<Point> &points) {
+  Point originalTop = points.top();
+  points.pop();
+  Point next = points.top();
+  points.push(originalTop);
+  return next;
+}
+
+/**
  * @brief Computes the angle of two points using 2nd argument arctangent.
- * 
- * @param a Point from where the angle takes reference. 
+ *
+ * @param a Point from where the angle takes reference.
  * @param b Point which indicates the angle between a and b.
  * @return double Value of angle in degrees.
  * Complexity: O(1).
@@ -34,60 +74,93 @@ double angleOfTwoPoints(Point a, Point b) {
   y = b.y - a.y;
   x = b.x - a.x;
   // Use inverse tangent for angle in radians.
-  result = atan2(y, x) * (180/3.1415);
+  result = atan2(y, x) * (180 / 3.1415);
   return result;
 }
 
 /**
  * @brief Computes the distance of two points.
- * 
+ *
  * @param p1 Point a.
  * @param p2 Point b.
- * @return double 
+ * @return double Distance of two points.
  * Complexity: O(1).
  */
-double distanceOfTwoPoints(Point &p1, Point &p2){
-  return sqrt(pow(p2.x-p1.x, 2) + pow(p2.y-p1.y, 2));
+double distanceOfTwoPoints(Point &p1, Point &p2) {
+  return sqrt(pow(p2.x - p1.x, 2) + pow(p2.y - p1.y, 2));
+}
+
+/**
+ * @brief Computes the orientation of two points towards a third point.
+ *
+ * @param a
+ * @param b
+ * @param c
+ * @return int Orientation.
+ * Complexity: O(1).
+ */
+int counterClockWiseDirection(Point a, Point b, Point c) {
+  float area = (b.x - a.x) * (c.y - a.y) - (b.y - a.y) * (c.x - a.x);
+  if (area > 0)
+    return 1; // Counter-clockwise.
+  // if (area < 0) return -1; // Clockwise
+  return 0; // Collinear
 }
 
 /**
  * @brief Algorith that finds the convex hull of a finite set of points.
- * 
+ *
  * @param points Vector contaning the points of a plane.
  * @return vector<Point>
- * Complexity: O(n log n). 
+ * Complexity: O(n log n).
  */
-vector<Point> grahamScan(vector<Point> points) {
+void grahamScan(vector<Point> points) {
   // Pick the bottom most, left most point.
-  Point lowest = points[0];
+  int lowest = 0;
   for (int i = 1; i < points.size(); i++) {
-    if (points[i].y < lowest.y) {
-      lowest = points[i];
-    } else if (points[i].y == lowest.y) {
-      if (points[i].x <= lowest.x) {
-        lowest = points[i];
+    if (points[i].y < points[lowest].y) {
+      lowest = i;
+    } else if (points[i].y == points[lowest].y) {
+      if (points[i].x <= points[lowest].x) {
+        lowest = i;
       }
     }
   }
   // Swap values with the first element of the vector with the lowest point.
-  swap(lowest, points[0]);
-  
+  swap(points[lowest], points[0]);
+
   // Sort by angle in respect to lowest point.
-  // First, compute the angle and distance in respect to lowest point, then sort.
+  // First, compute the angle and distance in respect to lowest point, then
+  // sort.
   for (int i = 0; i < points.size(); i++) {
-    points[i].angle = angleOfTwoPoints(lowest, points[i]);
-    points[i].distance = distanceOfTwoPoints(lowest, points[i]);
+    points[i].angle = angleOfTwoPoints(points[0], points[i]);
+    points[i].distance = distanceOfTwoPoints(points[0], points[i]);
   }
-  sort(points.begin(), points.end(), [](Point &p1, Point &p2){
-    if(p1.angle == p2.angle){
-      return p1.distance > p2.distance;
+  // Sort
+  sort(points.begin(), points.end(), [](Point &p1, Point &p2) {
+    if (p1.angle == p2.angle) {
+      return p1.distance < p2.distance;
     }
     return p1.angle < p2.angle;
   });
-
-  for (int i = 0; i < points.size(); i++) {
-    cout << i << ": " << points[i].x << ", " << points[i].y << "  Angle: " << points[i].angle << endl;
+  // Push the three first points.
+  stack<Point> polygon;
+  polygon.push(points[0]);
+  polygon.push(points[1]);
+  polygon.push(points[2]);
+  // Pop or push a point depending in its direction.
+  for (int i = 3; i < points.size(); i++) {
+    while (polygon.size() > 1 &&
+           counterClockWiseDirection(polygon.top(), nextToTop(polygon),
+                                     points[i])) {
+      polygon.pop();
+    }
+    polygon.push(points[i]);
   }
-  
-  return points;
+
+  if (polygon.size() < 3) {
+    cout << "=== This set of points does not have a convex hull ===" << endl;
+  } else {
+    printStack(polygon);
+  }
 }
--- activities/act4p2/main.cpp
+++ activities/act4p2/main.cpp
@@ -2,13 +2,15 @@
 Nombre: Actividad 4.2 - Implementación Polígonos Convexos aplicando geometría
 computacional. Descripción:
 - programa en C++ que implementa el algoritmo para determinar el polígono
-convexo más pequeño en un conjunto de puntos aplicando el algoritmo de Graham's
-Scan. 
+convexo más pequeño en un conjunto de puntos aplicando el algoritmo de
+Graham's Scan.
+
 Autores:
 - Jeshua Nava Avila | A01639282
 - Gael Rodriguez Gomez | A01639279
-Fecha de creación y última modificación:
-- 05/11/2022 - xx/11/2022
+
+\Fecha de creación y última modificación:
+- 05/11/2022 - 08/11/2022
 
 * Compilar
     g++ -std=c++17 main.cpp
@@ -31,11 +33,7 @@ int main(int argc, char *argv[]) {
     cin >> y;
     points.push_back(Point(x, y));
   }
-  for (int i = 0; i < points.size(); i++)
-  {
-    cout << "point " << i << ": " << points[i].x << "," << points[i].y << endl;
-  }
-  points = grahamScan(points);
-  
+  // Compute.
+  grahamScan(points);
   return 0;
 }
